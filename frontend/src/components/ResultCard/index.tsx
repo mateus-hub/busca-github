feccc7add84fc2814a6b318d3f85e348952fcc20
Mateus Silva <theusskt2018@gmail.com>
2021-09-03 14:06:01 -0300

    Test

--- frontend/src/components/ResultCard/index.tsx
+++ frontend/src/components/ResultCard/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const ResultCard = ({ title, description }: Props) => {
   return (
-    
+    //test
       <div className="result-container">  
         <h3 className="result-title">{title}</h3>
         <p className="result-description">{description}</p>
